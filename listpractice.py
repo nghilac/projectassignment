1626b77188edf29f41abb872e7ce8e3270b857ac
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-17 11:45:43 -0700

    Add files via upload
    Added file

---
+++ listpractice.py
@@ -0,0 +1,21 @@
+#               0           1           2           3           4           5        6          7           8           9
+city_list = ["Oakland","Atlanta", " NewYorkCity", "Seattle", "Memphis", "Miami", "Boston", "LosAngelas", "Denver", "NewOrleans"]
+
+city_list[0] = "SanFranciso"
+city_list[2] = "Brooklyn"
+city_list[7] = "HollyWoof"
+city_list[5] = "Tampa"
+print(city_list)
+
+threecities = (city_list[6:8])
+print(threecities)
+
+US_cities = city_list[5:9]
+print(US_cities)
+
+#               0       1       2       3       4       5           6          7        8       9
+animal_list = ["Dog","Cat", " Lion", "Bird", "Tiger", "Chicken", "Duck", "Lizard", "Seagull", "Otter"]
+print(animal_list)
+print(animal_list[4])
+
+
