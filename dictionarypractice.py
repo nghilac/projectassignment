0104e5d579e20e4c7c9044a3c8edbaa9cd399e1d
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-17 09:21:21 -0700

    adding a file

---
+++ dictionarypractice.py
@@ -0,0 +1,23 @@
+groceries = {"chicken":"$1.59" , "beef": "1.99" , "cheese": "1.00" , "milk": "2.50"}
+
+chicken_price = groceries = ["chicken"]
+print(chicken_price) 
+
+beef_price = groceries = ["beef"]
+print(beef_price) 
+
+cheese_price = groceries = ["cheese"]
+print(cheese_price) 
+
+milk_price = groceries = ["milk"]
+print(milk_price) 
+
+nbaplayers = {"LeBronJames": 30, "KevinDurant": 35, "StephenCurry": 30,"DamianLillard": 0}
+nbaplayers = {"LeBronJames": 23}
+jersey_number = nbaplayers["LeBronJames"]
+print (jersey_number)
+
+
+shoes = {"Jordan13": 1}
+
+animals = {"dogs": 5123, "cats": 12, "birds": 1, "lions": 41}  
