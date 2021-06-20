73f9660bb15782cec19c2bdb58377c4f5bb1e1f6
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-20 14:07:14 -0700

    Add files via upload
    Lab 3 pt 1

--- dictionarypractice.py
+++ dictionarypractice.py
@@ -1,16 +1,18 @@
-groceries = {"chicken":"$1.59" , "beef": "1.99" , "cheese": "1.00" , "milk": "2.50"}
+groceries = {"chicken":"1.59" , "beef": "1.99" , "cheese": "1.00" , "milk": "2.50"}
 
-chicken_price = groceries = ["chicken"]
+chicken_price = groceries["chicken"]
+chicken_price2 = groceries["chicken"]
 print(chicken_price) 
 
-beef_price = groceries = ["beef"]
+beef_price = groceries["beef"]
 print(beef_price) 
 
-cheese_price = groceries = ["cheese"]
+cheese_price = groceries["cheese"]
+
 print(cheese_price) 
 
-milk_price = groceries = ["milk"]
-print(milk_price) 
+milk_price = groceries["milk"]
+print(milk_price)  
 
 nbaplayers = {"LeBronJames": 30, "KevinDurant": 35, "StephenCurry": 30,"DamianLillard": 0}
 nbaplayers = {"LeBronJames": 23}
@@ -18,6 +20,8 @@ jersey_number = nbaplayers["LeBronJames"]
 print (jersey_number)
 
 
-shoes = {"Jordan13": 1}
+shoes = {"Jordan13": 1, "Yeezy": 8, "Foamposite": 10, "AirMax": 5, "SBDunk": 20}
+shoes = {"Jordan13": 5, "SBDunk": 22, "Yeezy": 13,"Foamposite": 14, "AirMax": 9, "SBDunk": 22}
+print(shoes)
 
 animals = {"dogs": 5123, "cats": 12, "birds": 1, "lions": 41}  
