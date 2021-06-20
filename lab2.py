4464ab1827ac7df40fae54aa4cc332b4ed0c6b1f
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-20 14:07:53 -0700

    Add files via upload
    lab 2

---
+++ lab2.py
@@ -0,0 +1,12 @@
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+car_name = "Ford"
+x = 50
+first_name = "Nghi"
+print(car_name)
+print(x)
+print(first_name)
