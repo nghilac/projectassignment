295dae61cfe07dbe60107f4594f01a691f53039c
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-20 14:39:57 -0700

    Lab 3 pt 2
    Lab 3- step VIII

--- listpractice.py
+++ listpractice.py
@@ -3,15 +3,25 @@ city_list = ["Oakland","Atlanta", " NewYorkCity", "Seattle", "Memphis", "Miami",
 
 city_list[0] = "SanFranciso"
 city_list[2] = "Brooklyn"
-city_list[7] = "HollyWoof"
+city_list[7] = "HollyWood"
 city_list[5] = "Tampa"
-print(city_list)
 
+city_list.pop(4)
+city_list.remove("Seattle")
+del city_list[1]
+ 
 threecities = (city_list[6:8])
 print(threecities)
 
 US_cities = city_list[5:9]
 print(US_cities)
+
+US_cities.append("Oakland")
+US_cities.extend(["NewYorkCity", "LosAngeles"])
+US_cities.insert(7,"Miami")
+
+print(city_list)
+
 
 #               0       1       2       3       4       5           6          7        8       9
 animal_list = ["Dog","Cat", " Lion", "Bird", "Tiger", "Chicken", "Duck", "Lizard", "Seagull", "Otter"]
