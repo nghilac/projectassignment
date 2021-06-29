63cdcd3237c02fbb512ca6615c7b32390a9b03fd
nghilac <85902064+nghilac@users.noreply.github.com>
2021-06-29 12:32:45 -0700

    Add files via upload
    updated version

--- whilelooppractice.py
+++ whilelooppractice.py
@@ -1,20 +1,40 @@
-def fibonacciFinder(max):
-    i = 0
-    j = 1
-    k = 0
-    while i < max:
-        print(k)
-        k = i + j
-        i = j 
-        j = k
-        return("fibonacci found!")
+groceries = {"chicken": 1.59 , "beef": 1.99 , "cheese": 1.00 , "milk": 2.50}
+shoes = {"Jordan13": 1, "Yeezy": 8, "Foamposite": 10, "AirMax": 5, "SBDunk": 20}
 
-def primeFinder(max):
-    for i in range(2, max):
-     for j in range(2,i):
-        if i % j == 0:
-          break 
-    else: 
-        print(i)
-    return(max)
-print(fibonacciFinder(100), primeFinder(15))
+
+def rectangleArea(width, height):
+    area = width * height
+    return area
+
+"chicken", "beef", "cheese", "milk"
+
+def total_price(item1, item2):
+    item1_price = groceries[item1]
+    item2_price = groceries[item2]
+    price = item1_price + item2_price
+
+    return price
+
+#print("chicken and cheese price")
+#print(total_price("chicken","cheese")) # 2.59
+
+print("chicken and cheese price " + str(total_price ("chicken", "cheese")))
+print("milk and beef price " + str(total_price ("milk", "cheese")))
+
+def price_difference(item1, item2):
+    item1_price = groceries[item1] #milk -> 2.5
+    item2_price = groceries[item2] #cheese -> 1.8
+
+    if item1_price > item2_price:
+        difference = item1_price - item2_price
+    else:
+        difference = item2_price - item1_price
+
+    #difference = item1_price - item2_price
+
+    return difference
+
+print(price_difference("chicken", "cheese"))
+print(price_difference("chicken", "milk"))
+
+
